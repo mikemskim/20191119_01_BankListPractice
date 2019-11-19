3a6fb5e3a489eb1bcf720c6c0dbd8f88fe11a52f
mikemskim <mskim@steconsulting.co.kr>
2019-11-19 21:02:21 +0900

    연습문제  - 택배 회사 목록을 받아서 리스트 뷰로 출력

---
+++ app/src/main/java/com/tj/a20191119_01_banklistpractice/DeliveryListActivity.kt
@@ -0,0 +1,22 @@
+package com.tj.a20191119_01_banklistpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeliveryListActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delivery_list)
+
+        setupEvent()
+        setValue()
+    }
+
+    override fun setupEvent() {
+    }
+
+    override fun setValue() {
+    }
+}
