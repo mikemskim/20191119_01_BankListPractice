0151c618fa2cb8b54a65643a9b5eecd07d229776
mikemskim <mskim@steconsulting.co.kr>
2019-11-19 20:21:00 +0900

    API

--- app/src/main/java/com/tj/a20191119_01_banklistpractice/MainActivity.kt
+++ app/src/main/java/com/tj/a20191119_01_banklistpractice/MainActivity.kt
@@ -2,7 +2,11 @@ package com.tj.a20191119_01_banklistpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.tj.a20191119_01_banklistpractice.datas.BankData
+import com.tj.a20191119_01_banklistpractice.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -24,6 +28,25 @@ class MainActivity : BaseActivity() {
     }
 
     fun getBanksFromServer() {
+        ServerUtil.getRequestBankList(mContext, object : ServerUtil.jsonResponseHandler {
+            override fun onResponse(json: JSONObject) {
+                Log.d("응답확인", json.toString())
 
+                val code = json.getInt("code")
+
+                if (code == 200) {
+                    val data = json.getJSONObject("data")
+                    val banks = data.getJSONArray("banks")
+
+                    for (i in 0..banks.length()-1) {
+                        var bank = banks.get(i)
+                    }
+
+                } else {
+                    Toast.makeText(mContext, "서버 통신에 문제가 있습니다.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+        })
     }
 }
--- app/src/main/java/com/tj/a20191119_01_banklistpractice/datas/BankData.kt
+++ app/src/main/java/com/tj/a20191119_01_banklistpractice/datas/BankData.kt
@@ -1,10 +1,23 @@
 package com.tj.a20191119_01_banklistpractice.datas
 
+import org.json.JSONObject
+
 class BankData(var id:Int, var code:String, var bankName:String, var bankLogoUrl:String) {
     init {
         id = 0
         code = ""
         bankName = ""
         bankLogoUrl = ""
+    }
+    
+    companion object {
+        
+        fun getBankFromJsonObject(json:JSONObject) : BankData {
+            var bank = BankData()
+            
+//           파싱 코드를 작성
+            
+            return bank
+        }
     }
 }
